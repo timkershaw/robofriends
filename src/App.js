935e5d2f3591de6d65a60975e050cf279871da39
timkershaw <44982931+timkershaw@users.noreply.github.com>
2022-04-13 14:17:44 -0400

    fetched jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
@@ -9,19 +8,28 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
-  render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
+onSearchChange = (event) => {
+  this.setState({ searchfield: event.target.value })
+}
+
+render() {
+  const filteredRobots = this.state.robots.filter(robots => {
+    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+  })
+  if (this.state.robots.length === 0) {
+    return <h1>Loading</h1>
+  } else {
     return (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
@@ -30,6 +38,7 @@ class App extends Component {
       </div>
     );
   }
+}
 }
 
 export default App;
